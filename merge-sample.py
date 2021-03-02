c274931dfa3225fcfb42427882bfceda156c2615
Spica TSUKIMI <haochengvanadis@gmail.com>
2021-03-02 18:23:47 +0800

    Update merge-sample.py

--- merge-sample.py
+++ merge-sample.py
@@ -1,6 +1,10 @@
 import os
 import imageio
+import argparse
 
+
+parser = argparse.ArgumentParser(description='CAAE')
+parser.add_argument('--dir', '-d', type=str)
 
 def mergegif(im, gifdir, interval=0.2):
     frames = []
@@ -11,9 +15,20 @@ def mergegif(im, gifdir, interval=0.2):
 
 
 if __name__ == '__main__':
-    pngdir=r'./save/samples/'
+    args = parser.parse_args()
+    if args.dir == 'samples':
+        pngdir = './save/samples/'
+        gifdir = './save/training_samples.gif'
+    elif args.dir == 'test':
+        pngdir = './save/test/'
+        gifdir = './save/test_outputs.gif'
+    else:
+        print("No such directory >_<")
+        exit()
     pngfiles = os.listdir(pngdir)
     # files.sort(key = lambda x:int(x[:-4]))
     image_list = [pngdir + img for img in pngfiles]
-    gifdir = './save/training_samples.gif'
+    if args.dir == 'test':
+        image_list = image_list[0:-1]
+        print(len(image_list))
     mergegif(image_list, gifdir)
